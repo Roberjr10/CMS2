2f40580548c64125e3d223e41566112ed6949483
Roberjr10 <91980134+Roberjr10@users.noreply.github.com>
2022-01-21 11:45:23 +0100

    archivo /public/index terminado carpeta helper terminada

--- helper/DbHelper.php
+++ helper/DbHelper.php
@@ -1,6 +1,26 @@
 <?php
 
-class DbHelper
-{
+namespace App\Helper;
+
+class DbHelper {
+
+    var $db;
+
+    function __construct(){
+
+        //Conexión mediante PDO
+        $opciones = [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
+        try {
+            $this->db = new \PDO(
+                'mysql:host=localhost;dbname=cms',
+                'rober-cms2',
+                'Rober_cms2',
+                $opciones);
+            $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+            echo 'Falló la conexión: ' . $e->getMessage();
+        }
+
+    }
 
 }
